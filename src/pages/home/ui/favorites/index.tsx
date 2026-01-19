30dc799ec3d27e4020b697279f7ba577930d167f
민경호 <1736s@naver.com>
2026-01-20 01:02:30 +0900

    design: 관심지역 목록 ui 마크업

--- src/pages/home/ui/favorites/index.tsx
+++ src/pages/home/ui/favorites/index.tsx
@@ -1,5 +1,23 @@
+import { Plus } from "lucide-react";
+
 const Favorites = () => {
-  return <div className="border border-black/20 rounded-lg">Favorites</div>;
+  return (
+    <div className="border border-black/20 rounded-lg">
+      <div className="p-4">
+        <strong>관심지역 목록</strong>
+      </div>
+      <hr className="border-black/20" />
+      <div className="p-4 flex flex-col gap-2 items-center">
+        <em>관심지역이 없습니다.</em>
+        <button
+          aria-label="관심지역 추가하기"
+          className="bg-blue-400 rounded-full p-1"
+        >
+          <Plus color="#fff" />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Favorites;
